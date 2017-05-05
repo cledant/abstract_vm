84a7f274c0f637c8c84bb20e7b2d893e15d93a7e
cledant <cledant@student.42.fr>
2017-05-05 11:07:48 +0200

    added overflow and underflow exception to class OperandFactory

--- includes/OperandFactory.hpp
+++ includes/OperandFactory.hpp
@@ -6,7 +6,7 @@
 /*   By: cledant <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/03 16:46:57 by cledant           #+#    #+#             */
-/*   Updated: 2017/05/04 14:14:52 by cledant          ###   ########.fr       */
+/*   Updated: 2017/05/05 11:03:33 by cledant          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,36 @@ class OperandFactory : public AFactory
 
 		OperandFactory(void);
 		virtual ~OperandFactory(void);
+
+		class	OverflowException : public std::exception
+		{
+			public :
+
+				explicit OverflowException(void);
+				virtual ~OverflowException(void) throw();
+				OverflowException(OverflowException const &src);
+				OverflowException &operator=(OverflowException const &rhs);
+				virtual const char		*what(void) const throw();
+
+			protected :
+
+				std::string		_msg;
+		};
+
+		class	UnderflowException : public std::exception
+		{
+			public :
+
+				explicit UnderflowException(void);
+				virtual ~UnderflowException(void) throw();
+				UnderflowException(UnderflowException const &src);
+				UnderflowException &operator=(UnderflowException const &rhs);
+				virtual const char		*what(void) const throw();
+
+			protected :
+
+				std::string		_msg;
+		};
 
 	private :
 
--- srcs/OperandFactory.cpp
+++ srcs/OperandFactory.cpp
@@ -6,7 +6,7 @@
 /*   By: cledant <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/03 17:23:40 by cledant           #+#    #+#             */
-/*   Updated: 2017/05/05 10:03:22 by cledant          ###   ########.fr       */
+/*   Updated: 2017/05/05 11:05:06 by cledant          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,4 +30,58 @@ IOperand const		*OperandFactory::createInt16(std::string const &value) const
 	else if (convert < std::numeric_limits<short int>::min())
 		throw std::underflow_error("Creating this variable would cause an underflow");
 	return (new OperandInt16(static_cast<short int>(convert)));
+}
+
+OperandFactory::OverflowException::OverflowException(void)
+{
+	this->_msg = "Creating this variable would cause an overflow !";
+}
+
+OperandFactory::OverflowException::~OverflowException(void) throw()
+{
+}
+
+OperandFactory::OverflowException::OverflowException(OverflowException const &src)
+{
+	this->_msg = src._msg.c_str();
+}
+
+OperandFactory::OverflowException		&OperandFactory::OverflowException::
+											operator=(OverflowException const &rhs)
+{	
+	this->_msg = rhs._msg.c_str();
+	return (*this);
+}
+
+const char								*OperandFactory::OverflowException::what(
+											void) const throw()
+{
+	return (this->_msg.c_str());
+}
+
+OperandFactory::UnderflowException::UnderflowException(void)
+{
+	this->_msg = "Creating this variable would cause an underflow !";
+}
+
+OperandFactory::UnderflowException::~UnderflowException(void) throw()
+{
+}
+
+OperandFactory::UnderflowException::UnderflowException(UnderflowException const &src)
+{
+	this->_msg = src._msg.c_str();
+}
+
+OperandFactory::UnderflowException	&OperandFactory::UnderflowException::operator=(
+										UnderflowException const &rhs)
+{	
+	this->_msg = rhs._msg.c_str();
+	return (*this);
+}
+
+const char							*OperandFactory::UnderflowException::what(void)
+										const throw()
+{
+	return (this->_msg.c_str());
 }
